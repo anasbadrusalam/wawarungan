c4a17708fc955a2ed12488e2c0db523cc0363a63
Badru Salam Anas <badrusalamanas@gmail.com>
2026-04-22 11:28:17 +0700

    WIP

--- app/Filament/Resources/Products/Pages/ViewProduct.php
+++ app/Filament/Resources/Products/Pages/ViewProduct.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\Products\Pages;
 
 use App\Filament\Resources\Products\ProductResource;
+use Filament\Actions\CreateAction;
 use Filament\Actions\EditAction;
 use Filament\Resources\Pages\ViewRecord;
 
@@ -13,6 +14,7 @@ class ViewProduct extends ViewRecord
     protected function getHeaderActions(): array
     {
         return [
+            CreateAction::make(),
             EditAction::make(),
         ];
     }
