5984f2e7daba595f2dbf6412ba88f8611a14f968
Badru Salam Anas <badrusalamanas@gmail.com>
2026-04-26 16:24:51 +0700

    WIP

--- app/Filament/Resources/Brands/BrandResource.php
+++ app/Filament/Resources/Brands/BrandResource.php
@@ -15,10 +15,13 @@ use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
+use UnitEnum;
 
 class BrandResource extends Resource
 {
     protected static ?string $model = Brand::class;
+
+    protected static string | UnitEnum | null $navigationGroup = 'Products';
 
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
 
--- app/Filament/Resources/Categories/CategoryResource.php
+++ app/Filament/Resources/Categories/CategoryResource.php
@@ -15,10 +15,13 @@ use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
+use UnitEnum;
 
 class CategoryResource extends Resource
 {
     protected static ?string $model = Category::class;
+
+    protected static string | UnitEnum | null $navigationGroup = 'Products';
 
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
 
