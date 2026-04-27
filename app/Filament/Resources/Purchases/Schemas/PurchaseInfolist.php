49bba1493c83f0d87593004fc3be27d7b3ce6b5c
Badru Salam Anas <badrusalamanas@gmail.com>
2026-04-27 16:12:49 +0700

    WIP

--- app/Filament/Resources/Purchases/Schemas/PurchaseInfolist.php
+++ app/Filament/Resources/Purchases/Schemas/PurchaseInfolist.php
@@ -2,7 +2,10 @@
 
 namespace App\Filament\Resources\Purchases\Schemas;
 
+use Filament\Forms\Components\Repeater\TableColumn;
+use Filament\Infolists\Components\RepeatableEntry;
 use Filament\Infolists\Components\TextEntry;
+use Filament\Schemas\Components\Grid;
 use Filament\Schemas\Schema;
 
 class PurchaseInfolist
@@ -14,12 +17,43 @@ class PurchaseInfolist
                 TextEntry::make('supplier.name')
                     ->label('Supplier'),
                 TextEntry::make('status'),
-                TextEntry::make('ordered_at')
-                    ->date()
-                    ->placeholder('-'),
-                TextEntry::make('received_at')
-                    ->date()
-                    ->placeholder('-'),
+                RepeatableEntry::make('items')
+                    ->dense()
+                    ->columnSpanFull()
+                    ->label('Products')
+                    ->columns([
+                        'default' => 2,
+                        'sm' => 2,
+                    ])
+                    ->schema([
+                        TextEntry::make('product.name')
+                            ->wrap()
+                            ->label('Product'),
+                        Grid::make([
+                            'default' => 6,
+                            'sm' => 6,
+                        ])
+                            ->dense()
+                            ->schema([
+                                TextEntry::make('quantity')
+                                    ->badge()
+                                    ->color('info')
+                                    ->size('large')
+                                    ->columnSpan([
+                                        'default' => 2,
+                                        'sm' => 2,
+                                    ]),
+                                TextEntry::make('price')
+                                    ->columnSpan([
+                                        'default' => 4,
+                                        'sm' => 4,
+                                    ])
+                                    ->badge()
+                                    ->color('danger')
+                                    ->size('large')
+                                    ->money(locale: 'id', currency: 'IDR'),
+                            ]),
+                    ]),
                 TextEntry::make('created_at')
                     ->dateTime()
                     ->placeholder('-'),
